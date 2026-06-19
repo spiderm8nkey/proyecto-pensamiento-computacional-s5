a90a18c4818d7472a5f0edeaf0687bd27793c694
spiderm8nkey <franco.moya@mail.udp.cl>
2026-06-19 15:26:10 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textAlign(CENTER, CENTER);
+  textFont('TimesNewRoman');
+  textSize(24)
+  text("Aqui estará el examen",width/2,height/2);
+}
